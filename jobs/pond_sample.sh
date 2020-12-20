25a2a79f711c9f80ea93ab692c469cd8cdf748ef
Amartya Das Sharma <amartyads@gmail.com>
2020-12-20 15:45:04 +0100

    trial jobscript

---
+++ jobs/pond_sample.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J pond_sample
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./../build/
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=ga53qud@mytum.de
+#SBATCH --export=NONE
+#SBATCH --time=01:00:00
+  
+module load slurm_setup
+
+module use -p /lrz/sys/spack/.tmp/test/gpi2/modules/x86_avx2/linux-sles12-x86_64
+module load gpi-2/1.4.0-gcc8-impi-rdma-core-24
+module load cmake/3.16.5
+module load metis/5.1.0-intel19-i32-r32
+module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
+module load gcc/8
+
+mpiexec -n 16 ./pond -x 16384 -y 16384 -p 2048 -e 1 -c 1 --scenario 2 -o output/out
