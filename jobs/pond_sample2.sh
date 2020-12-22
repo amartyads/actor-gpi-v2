c6c0818a0147b69119b6146b17a94b4d75f245ff
Amartya Das Sharma <amartyads@gmail.com>
2020-12-22 18:22:03 +0100

    bigger datablocks, back to debug library, logger

--- jobs/pond_sample2.sh
+++ jobs/pond_sample2.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ga53qud@mytum.de
 #SBATCH --export=NONE
-#SBATCH --time=00:10:00
+#SBATCH --time=03:00:00
   
 module load slurm_setup
 
@@ -21,4 +21,5 @@ module load metis/5.1.0-intel19-i32-r32
 module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
 module load gcc/8
 
-mpiexec -n 112 ./pond -x 16384 -y 16384 -p 2048 -e 1 -c 1 --scenario 2 -o output/out
+gaspi_logger &
+mpiexec -n 112 --perhost 28 ./pond -x 16384 -y 16384 -p 2048 -e 1 -c 1 --scenario 2 -o output/out
