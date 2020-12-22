069322a6e62dc7d931222ce62582ed2c8e81d85c
Amartya Das Sharma <ga53qud2@cm2login2.cos.lrz.de>
2020-12-22 18:14:53 +0100

    updated batchfile

--- jobs/pond_sample2.sh
+++ jobs/pond_sample2.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH -J pond_proper
+#SBATCH -J pond_128
 #SBATCH -o ./%x.%j.%N.out
-#SBATCH -D ./
+#SBATCH -D ./../build/
 #SBATCH --get-user-env
 #SBATCH --clusters=cm2
 #SBATCH --partition=cm2_std
-#SBATCH --nodes=8
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=28
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ga53qud@mytum.de
 #SBATCH --export=NONE
-#SBATCH --time=05:00:00
+#SBATCH --time=00:10:00
   
 module load slurm_setup
 
@@ -20,6 +21,4 @@ module load metis/5.1.0-intel19-i32-r32
 module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
 module load gcc/8
 
-cd ../build/
-
-mpiexec -n 8 --perhost 1 ./pond -x 16384 -y 16384 -p 2048 -e 1 -c 1 --scenario 2 -o output/out
+mpiexec -n 112 ./pond -x 16384 -y 16384 -p 2048 -e 1 -c 1 --scenario 2 -o output/out
--- jobs/pond_smaller_sample.sh
+++ jobs/pond_smaller_sample.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH -J pond_sample
+#SBATCH -J pond_small
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./../build/
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2
-#SBATCH --partition=cm2_std
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=1
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+#SBATCH --nodes=2
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ga53qud@mytum.de
 #SBATCH --export=NONE
@@ -21,4 +20,4 @@ module load metis/5.1.0-intel19-i32-r32
 module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
 module load gcc/8
 
-mpiexec -n 4 ./pond -x 128 -y 128 -p 16 -e 1 -c 1 --scenario 2 -o output/out
+mpiexec -n 56 ./pond -x 1024 -y 1024 -p 128 -e 1 -c 1 --scenario 2 -o output/out
