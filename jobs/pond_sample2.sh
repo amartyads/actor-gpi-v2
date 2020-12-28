c743bad36205d44c809eabbf27868d07981ddd05
Amartya Das Sharma <amartyads@gmail.com>
2020-12-28 23:11:12 +0100

    jobfile change

--- jobs/pond_sample2.sh
+++ jobs/pond_sample2.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=ga53qud@mytum.de
 #SBATCH --export=NONE
-#SBATCH --time=03:00:00
+#SBATCH --time=00:20:00
   
 module load slurm_setup
 
@@ -20,6 +20,8 @@ module load cmake/3.16.5
 module load metis/5.1.0-intel19-i32-r32
 module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
 module load gcc/8
+
+export OMP_NUM_THREADS=1
 
 gaspi_logger &
 mpiexec -n 112 --perhost 28 ./pond -x 16384 -y 16384 -p 512 -e 1 -c 1 --scenario 2 -o output/out
