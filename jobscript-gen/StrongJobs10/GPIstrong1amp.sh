37b63c821373dfe859a08f47c18e9c01f0e0bf20
Amartya Das Sharma <ga53qud2@cm2login3.cos.lrz.de>
2021-03-17 02:24:40 +0100

    final push with all outputs

--- jobscript-gen/StrongJobs10/GPIstrong1amp.sh
+++ jobscript-gen/StrongJobs10/GPIstrong1amp.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J GPIstrong32
+#SBATCH -J GPIstrong1
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2
-#SBATCH --partition=cm2_large
-#SBATCH --qos=cm2_large
-#SBATCH --nodes=32
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+# no qos specification flag needed
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
-#SBATCH --exclude=i22r06c05s[09-10],i22r04c04s10,i22r06c03s07,i22r02c05s12,i22r03c04s04,i22r02c05s10,i22r02c01s05,i22r04c01s05,i22r04c01s12,i22r05c04s03,i22r05c01s01,i22r01c01s12,i22r01c06s12,i22r01c06s12
+# no nodes need to be excluded
 #SBATCH --mail-type=all
 #SBATCH --mail-user=ga53qud@mytum.de
 #SBATCH --export=NONE
@@ -21,10 +21,11 @@ module load gpi-2/1.4.0-gcc8-impi-rdma-core-24
 module load metis/5.1.0-intel19-i32-r32
 module load netcdf-hdf5-all/4.6_hdf5-1.10-intel19-impi
 module load gcc/8
+module load amplifier_xe
 
 cd /dss/dsshome1/lxc06/ga53qud2/actor-gpi-v2/build
 
 export OMP_NUM_THREADS=1
 
 gaspi_logger &
-mpiexec -n 320 --perhost 10 ./pond -x 16384 -y 16384 -p 256 -e 1 -c 1 --scenario 2 -o output/out
+mpiexec -n 10 --perhost 10 amplxe-cl --collect hotspots -r /dss/dsshome1/lxc06/ga53qud2/actor-gpi-v2/amplifier-analysis/GPIstrong1noflux/ampli ./pond -x 16384 -y 16384 -p 1024 -e 1 -c 1 --scenario 2 -o output/out
